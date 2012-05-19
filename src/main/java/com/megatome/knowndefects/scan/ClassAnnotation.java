dd52dc61f833a70a2b8cd009d56154ffe548ade5
Chad Johnston <cjohnston@megatome.com>
2012-05-18 20:20:06 -0600

    New class for data structure

---
+++ src/main/java/com/megatome/knowndefects/scan/ClassAnnotation.java
@@ -0,0 +1,11 @@
+package com.megatome.knowndefects.scan;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chad
+ * Date: 5/18/12
+ * Time: 6:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClassAnnotation {
+}
