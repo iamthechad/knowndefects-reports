10f3aaa1a4db4ec19f881ebce41a6ae215e91cbe
Chad Johnston <cjohnston@megatome.com>
2012-05-20 19:48:35 -0600

    Add new class

---
+++ src/main/java/com/megatome/knowndefects/scan/PackageScanResults.java
@@ -0,0 +1,11 @@
+package com.megatome.knowndefects.scan;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chad
+ * Date: 5/20/12
+ * Time: 6:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PackageScanResults {
+}
